3655890cdfb0cdb6007b136b63135ae779847770
Mindaugas <mindaugasgrybauskas@protonmail.com>
2022-11-08 16:27:42 +0100

    Create functions for all of the basic math operators

---
+++ java.js
@@ -0,0 +1,28 @@
+
+function add (firstNumber, secondNumber){
+    firstNumber = prompt('Pick a number');
+    secondNumber = prompt('pick another number');
+    const sum = (+firstNumber) + (+secondNumber);
+    console.log(sum);
+}
+
+function subtract (firstNumber, secondNumber){
+    firstNumber = prompt('Pick a number');
+    secondNumber = prompt('pick another number');
+    const sum = (+firstNumber) - (+secondNumber);
+    console.log(sum);
+}
+
+function multiply (firstNumber, secondNumber){
+    firstNumber = prompt('Pick a number');
+    secondNumber = prompt('pick another number');
+    const sum = (+firstNumber) * (+secondNumber);
+    console.log(sum);
+}
+
+function divide (firstNumber, secondNumber){
+    firstNumber = prompt('Pick a number');
+    secondNumber = prompt('pick another number');
+    const sum = (+firstNumber) / (+secondNumber);
+    console.log(sum);
+}
