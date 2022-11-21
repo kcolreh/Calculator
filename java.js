da971611a63b6e29a179285d6dfb0ff7ed2e65b2
Mindaugas <mindaugasgrybauskas@protonmail.com>
2022-11-21 16:51:44 +0100

    Make code readable, create keyboard compatibility, transpose some code into functions

--- java.js
+++ java.js
@@ -11,9 +11,17 @@ const equals = document.getElementById('equals');
 const backspace = document.getElementById('clearOne');
 const dotButton = document.getElementById('dot');
 
+add.addEventListener('click', addClick);
+subtract.addEventListener('click', subtractClick);
+multiply.addEventListener('click', multiplyClick);
+divide.addEventListener('click', divideClick);
+clear.addEventListener('click', clearAll);
+backspace.addEventListener('click', deleteDigit);
+equals.addEventListener('click', equalsAl);
+dotButton.addEventListener('click', makeDecimals);
+
 let firstNumber = ''; // First Variable
 let secondNumber = ''; // Second Variable
-
 
 //Gets input from buttons and displays it on the screen.
 buttons.forEach(button => {
@@ -48,7 +56,65 @@ buttons.forEach(button => {
     })
 });
 
-dotButton.addEventListener('click', function () {
+document.addEventListener('keydown', (event) => {
+    if (event.code === 'Digit8' && event.shiftKey) {
+        document.getElementById('multiply').click();
+
+    } else if (event.code === 'Minus') {
+        document.getElementById('subtract').click();
+
+    } else if (event.code === 'Slash') {
+        document.getElementById('divide').click();
+
+    } else if (event.code === 'Equal' && event.shiftKey) {
+        document.getElementById('add').click();
+
+    } else if (event.code === 'Equal') {
+        document.getElementById('equals').click();
+
+    } else if (event.code === 'Period') {
+        document.getElementById('dot').click();
+
+    } else if (event.code === 'Backspace') {
+        document.getElementById('clearOne').click();
+
+    } else if (event.code === 'Delete') {
+        document.getElementById('clear').click();
+
+    } else if (event.code === 'Digit1') {
+        document.getElementById('one').click();
+
+    } else if (event.code === 'Digit2') {
+        document.getElementById('two').click();
+
+    } else if (event.code === 'Digit3') {
+        document.getElementById('three').click();
+
+    } else if (event.code === 'Digit4') {
+        document.getElementById('four').click();
+
+    } else if (event.code === 'Digit5') {
+        document.getElementById('five').click();
+
+    } else if (event.code === 'Digit6') {
+        document.getElementById('six').click();
+
+    } else if (event.code === 'Digit7') {
+        document.getElementById('seven').click();
+
+    } else if (event.code === 'Digit8') {
+        document.getElementById('eight').click();
+
+    } else if (event.code === 'Digit9') {
+        document.getElementById('nine').click();
+
+    } else if (event.code === 'Digit0') {
+        document.getElementById('zero').click();
+    }
+})
+
+//Puts a . between numbers to create decimals
+function makeDecimals() {
     dot.value = '.';
 
     if (display.textContent.includes(dot.value)) { //Checks if the main display has a dot
@@ -57,12 +123,9 @@ dotButton.addEventListener('click', function () {
         dot.value = '.'; //Lets make you a new dot if theres none
     }
     display.textContent += dot.value; //display the dot with the main number and operator inputs
-})
-
+}
 
 //calls for the functions to get the answer
-equals.addEventListener('click', equalsAl)
-
 function equalsAl() {
     if (display2.textContent.includes('+')) { //Detecs with Operator is in the string and calls the correct function
         addFunc(firstNumber, secondNumber)
@@ -74,83 +137,6 @@ function equalsAl() {
         divideFunc(firstNumber, secondNumber);
     }
 }
-
-//Operator buttons
-add.addEventListener('click', () => {
-    if (typeof firstNumber === 'number' && secondNumber === '') { //Allows to chain multiple operations in a row
-        display.textContent = '';
-        return display2.textContent = firstNumber + ' +';
-    } else if (display.textContent === '' && display2.textContent === '') { //Dosen't allow user to put in an operator before a number is selected
-        return display.textContent = ''
-    } else if (firstNumber != '' && secondNumber != '') { //Calls for an input evaluation if both numbers are selected and another operator is pressed
-        equalsAl()
-    }
-    display2.textContent = firstNumber + ' +';
-    display.textContent = '';
-});
-
-subtract.addEventListener('click', () => {
-    if (typeof firstNumber === 'number' && secondNumber === '') { //Allows to chain multiple operations in a row
-        display.textContent = '';
-        return display2.textContent = firstNumber + ' -';
-    } else if (display.textContent === '' && display2.textContent === '') { //Dosen't allow user to put in an operator before a number is selected
-        return display.textContent = ''
-    } else if (firstNumber != '' && secondNumber != '') { //Calls for an input evaluation if both numbers are selected and another operator is pressed
-        equalsAl()
-    }
-    display2.textContent = firstNumber + ' -';
-    display.textContent = '';
-});
-
-multiply.addEventListener('click', () => {
-    if (typeof firstNumber === 'number' && secondNumber === '') { //Allows to chain multiple operations in a row
-        display.textContent = '';
-        return display2.textContent = firstNumber + ' x';
-    } else if (display.textContent === '' && display2.textContent === '') { //Dosen't allow user to put in an operator before a number is selected
-        return display.textContent = ''
-    } else if (firstNumber != '' && secondNumber != '') { //Calls for an input evaluation if both numbers are selected and another operator is pressed
-        equalsAl()
-    }
-    display2.textContent = firstNumber + ' x';
-    display.textContent = '';
-});
-
-divide.addEventListener('click', () => {
-    if (typeof firstNumber === 'number' && secondNumber === '') { //Allows to chain multiple operations in a row
-        display.textContent = '';
-        return display2.textContent = firstNumber + ' /';
-    } else if (display.textContent === '' && display2.textContent === '') { //Dosen't allow user to put in an operator before a number is selected
-        return display.textContent = ''
-    } else if (firstNumber != '' && secondNumber != '') { //Calls for an input evaluation if both numbers are selected and another operator is pressed
-        equalsAl()
-    }
-    display2.textContent = firstNumber + ' /';
-    display.textContent = '';
-});
-
-//Clears the displays and values
-clear.addEventListener('click', () => {
-    display.textContent = ''; // Clears main display
-    display2.textContent = ''; // Clears secondary display
-    firstNumber = ''; // Clears first variable
-    secondNumber = ''; // Clears second variable
-});
-
-
-
-//Clears single character from displays
-backspace.addEventListener('click', () => {
-    let deleteOne = display.textContent;
-    let deleteTwo = display2.textContent;
-    if (display2.textContent === '') {
-        display.textContent = deleteOne.slice(1); //Deletes one character from Main Display
-    } else if (display.textContent === '') {
-        display2.textContent = deleteTwo.slice(0, -1); //Deletes one character from Secondary Display
-    } else if (display.textContent != '' && display2.textContent != '') { // Allows for function to work if both displays have an input
-        display.textContent = deleteOne.slice(1);
-    }
-})
-
 
 //Functions to calculate the inputs
 function addFunc(firstNumberAdd, secondNumberAdd) {
@@ -202,6 +188,80 @@ function divideFunc(firstNumberDivide, secondNumberDivide) {
     display.textContent = Math.round(sum * 1000) / 1000; // Rounds up the answer to have maximum of 3 decimals
     firstNumber = Math.round(sum * 1000) / 1000; // Rounds up the answer to have maximum of 3 decimals
     secondNumber = '';
+}
+
+//Clears the displays and values
+function clearAll() {
+    display.textContent = ''; // Clears main display
+    display2.textContent = ''; // Clears secondary display
+    firstNumber = ''; // Clears first variable
+    secondNumber = ''; // Clears second variable
+}
+
+//Clears single character from displays
+function deleteDigit() {
+    let deleteOne = display.textContent;
+    let deleteTwo = display2.textContent;
+    if (display2.textContent === '') {
+        display.textContent = deleteOne.slice(0, -1); //Deletes one character from Main Display
+    } else if (display.textContent === '') {
+        display2.textContent = deleteTwo.slice(0, -1); //Deletes one character from Secondary Display
+    } else if (display.textContent != '' && display2.textContent != '') { // Allows for function to work if both displays have an input
+        display.textContent = deleteOne.slice(0, -1);
+    }
+}
+
+//Operator buttons
+function divideClick() {
+    if (typeof firstNumber === 'number' && secondNumber === '') { //Allows to chain multiple operations in a row
+        display.textContent = '';
+        return display2.textContent = firstNumber + ' /';
+    } else if (display.textContent === '' && display2.textContent === '') { //Dosen't allow user to put in an operator before a number is selected
+        return display.textContent = ''
+    } else if (firstNumber != '' && secondNumber != '') { //Calls for an input evaluation if both numbers are selected and another operator is pressed
+        equalsAl()
+    }
+    display2.textContent = firstNumber + ' /';
+    display.textContent = '';
+}
+
+function addClick() {
+    if (typeof firstNumber === 'number' && secondNumber === '') { //Allows to chain multiple operations in a row
+        display.textContent = '';
+        return display2.textContent = firstNumber + ' +';
+    } else if (display.textContent === '' && display2.textContent === '') { //Dosen't allow user to put in an operator before a number is selected
+        return display.textContent = ''
+    } else if (firstNumber != '' && secondNumber != '') { //Calls for an input evaluation if both numbers are selected and another operator is pressed
+        equalsAl()
+    }
+    display2.textContent = firstNumber + ' +';
+    display.textContent = '';
+}
+
+function subtractClick() {
+    if (typeof firstNumber === 'number' && secondNumber === '') { //Allows to chain multiple operations in a row
+        display.textContent = '';
+        return display2.textContent = firstNumber + ' -';
+    } else if (display.textContent === '' && display2.textContent === '') { //Dosen't allow user to put in an operator before a number is selected
+        return display.textContent = ''
+    } else if (firstNumber != '' && secondNumber != '') { //Calls for an input evaluation if both numbers are selected and another operator is pressed
+        equalsAl()
+    }
+    display2.textContent = firstNumber + ' -';
+    display.textContent = '';
+}
+
+function multiplyClick() {
+    if (typeof firstNumber === 'number' && secondNumber === '') { //Allows to chain multiple operations in a row
+        display.textContent = '';
+        return display2.textContent = firstNumber + ' x';
+    } else if (display.textContent === '' && display2.textContent === '') { //Dosen't allow user to put in an operator before a number is selected
+        return display.textContent = ''
+    } else if (firstNumber != '' && secondNumber != '') { //Calls for an input evaluation if both numbers are selected and another operator is pressed
+        equalsAl()
+    }
+    display2.textContent = firstNumber + ' x';
+    display.textContent = '';
 }
 
 
